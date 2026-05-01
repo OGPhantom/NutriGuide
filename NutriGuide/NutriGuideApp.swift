7fd83d002ccf44601022096c5d15db52caebc94b
OGPhantom <nikita.storchai@nure.ua>
2026-05-01 20:13:11 +0300

    Initial commit

---
+++ NutriGuide/NutriGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriGuideApp.swift
+//  NutriGuide
+//
+//  Created by Никита Сторчай on 01.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct NutriGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
